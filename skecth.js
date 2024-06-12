122eceb4719f33110062cff8104e25760f4741a6
js-ana caroline <00001088929345sp@al.educacao.sp.gov.br>
2024-06-12 18:12:41 -0300

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -1,13 +1,14 @@
 function setup() {
   createCanvas(400, 400);
   background("#FFC107")
-}
+} 
+
 function draw() {
   stroke("blue");
   fill("red");
-  
-  
-     if (mouseIsPressed) {
-        rect(mouseX,mouseY, 20,35);
-     }
+
+
+   if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
 }
